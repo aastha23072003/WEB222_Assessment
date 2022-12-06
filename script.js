6eaf0a2359c24ba99805653a6f5e67b191d6ac7a
aastha23072003 <119976846+aastha23072003@users.noreply.github.com>
2022-12-06 18:11:49 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-          $("#show").click(function () {
-            $("#Create").show();
-          });
-        });
-    document.getElementById()
-        
-
-    
